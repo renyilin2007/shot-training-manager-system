61bb345fe74fddb40843d8a1b18cbeb8fa3884fc
Ren Yilin <renyilin@yuanwang.com>
2021-01-29 11:10:58 +0800

    绘图：确定了中心;任意点绘制在测

--- src/modules/devices/Target.cpp
+++ src/modules/devices/Target.cpp
@@ -1,5 +1,7 @@
 #include "Target.h"
 #include "ui_Target.h"
+#include "ui_Target.h"
+#include "QDebug.h"
 
 #include "simutarget.h"
 extern SimuTarget *g_pSimuTar;
@@ -9,8 +11,6 @@ Target::Target(QWidget *parent) :
     ui(new Ui::Target)
 {
     ui->setupUi(this);
-
-    m_y = 10;
 }
 
 Target::~Target()
@@ -31,17 +31,37 @@ void Target::connectSignal()
 
 void Target::paintEvent(QPaintEvent *event)
 {
-    QPainter painter(this);  //创建一位画�?
-    QPen pen(QColor(255,0,0));  //创建画笔
-    pen.setWidth(3);  //笔宽
-    painter.setPen(pen);  //给画家一根笔
+	QPainter painter(this);
+	QPen pen(QColor(255,0,0));	//创建画笔
+	pen.setWidth(5);  //笔宽
+	painter.setPen(pen);  //给画家一根笔
+	QPixmap pixmap(":/pic/targets/101-50胸环�?bmp");
+	painter.drawPixmap(0,0,pixmap);
+	qDebug()<<"pixmap.width:"<<pixmap.width();
+	qDebug()<<"pixmap.widthMM:"<<pixmap.widthMM();
+	qDebug()<<"pixmap.height:"<<pixmap.height();
+	qDebug()<<"pixmap.heightMM:"<<pixmap.heightMM();
 
-    QPixmap pixmap;
-    pixmap.load(":/pic/targets/101-50胸环�?bmp");
-    int width = pixmap.width();
-    int height = pixmap.height();
-    resize(width, height);
-    painter.drawPixmap(0,0,pixmap);//图片原始大小输出
+	resize(pixmap.width(), pixmap.height());
 
-    painter.drawPoint(m_pos.x(), m_pos.y());
+	int width = pixmap.width();
+	int widthMM = pixmap.widthMM();
+	int height = pixmap.height();
+	int heightMM = pixmap.heightMM();
+	int center_pos_w = pixmap.width()/2;
+	int center_pos_h = pixmap.height()*30/(30+20);
+	int center_pos_w_MM = pixmap.widthMM();
+	int center_pos_h_MM = pixmap.heightMM();
+
+	painter.drawPoint(center_pos_w, center_pos_h);
+
+	//(10,-20)mm
+	int point_pos_w = center_pos_w + (10/center_pos_w_MM)*center_pos_w;
+	int point_pos_h = center_pos_h + (20/(widthMM-center_pos_h))*(height-center_pos_h);
+	qDebug()<<"point_pos_w:"<<point_pos_w;
+	qDebug()<<"point_pos_h:"<<point_pos_h;
+	painter.drawPoint(point_pos_w, point_pos_h);
+	//(120,-40)mm
+
 }
+
